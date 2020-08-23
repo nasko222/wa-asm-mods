c803408fe36d878e4ebdd896d4daf8834a20c442
nasko222 <nasko222bg@abv.bg>
2020-08-23 16:03:26 +0300

    New mod: WA Editor 0.96 limitation remover

---
+++ wa096-limitation-remover/colorrem.asm
@@ -0,0 +1,31 @@
+startcolorrem:
+    cmp ebx, 32767
+jmp endcolorrem
+
+startcolorrem2:
+    cmp ebx, -32768
+jmp endcolorrem2
+
+startcolorrem3:
+    mov esi, 0
+jmp endcolorrem3
+
+startcolorrem4:
+    cmp ebx, 32767
+jmp endcolorrem4
+
+startcolorrem5:
+    cmp ebx, -32768
+jmp endcolorrem5
+
+startcolorrem6:
+    mov esi, 0
+jmp endcolorrem6
+
+startcolorrem7:
+    cmp ebx, 7
+jmp endcolorrem7
+
+startcolorrem8:
+    mov esi, 7
+jmp endcolorrem8
---
+++ wa096-limitation-remover/gloverem.asm
@@ -0,0 +1,31 @@
+startgloverem:
+    cmp ebx, -32768
+jmp endgloverem
+
+startgloverem2:
+    mov esi, 0
+jmp endgloverem2
+
+startgloverem3:
+    cmp ebx, 32767
+jmp endgloverem3
+
+startgloverem4:
+    mov esi, 0
+jmp endgloverem4
+
+startgloverem5:
+    cmp ebx, -32768
+jmp endgloverem5
+
+startgloverem6:
+    mov esi, 0
+jmp endgloverem6
+
+startgloverem7:
+    cmp ebx, 32767
+jmp endgloverem7
+
+startgloverem8:
+    mov esi, 0
+jmp endgloverem8
