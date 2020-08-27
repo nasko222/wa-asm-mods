61ffc90639f51117e0ac5108c5a080b24ec76228
nasko222 <nasko222bg@abv.bg>
2020-08-27 13:23:52 +0300

    Update 1.01
    Bug Fixes:
    Fixed the bug where the timed button can't go above sub color 4
    
    Additions:
    Added unused model frame 3
    Added support for negative FN for custom items (the integer is byte so its 0 - 255)

---
+++ wa096-limitation-remover/update101.asm
@@ -0,0 +1,11 @@
+startframethreefix:
+    cmp ebx, 3
+jmp endframethreefix
+
+startframethreefix2:
+    cmp ebx, 0
+jmp endframethreefix2
+
+startframethreefix3:
+    mov esi, 3
+jmp endframethreefix3
